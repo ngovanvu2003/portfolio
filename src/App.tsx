f194a9e88aaf477c49b9f2e642aaa52024215bee
ngovanvu203 <128632824+ngovanvu2003@users.noreply.github.com>
2023-07-12 05:33:29 +0700

    tạo file

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './assets/client/root.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
