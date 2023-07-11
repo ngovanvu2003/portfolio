87ebcbd75419cbd4e2f6af652899527d7f00f32a
ngovanvu203 <128632824+ngovanvu2003@users.noreply.github.com>
2023-07-12 06:48:55 +0700

    update skill page

--- src/pages/client/skills/skill.tsx
+++ src/pages/client/skills/skill.tsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 const Skill = () => {
   return (
-    <div>Skill</div>
+    <div id="skill">
+    <h1 className="title">SKILL</h1>
+    <div className="listSkill">
+      <div className="item">
+        <div className="name">HTML5</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+      <div className="item">
+        <div className="name">CSS3</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+      <div className="item">
+        <div className="name">Javascript</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+      <div className="item">
+        <div className="name">PHP</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+
+      <div className="item">
+        <div className="name">React JS</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+      <div className="item">
+        <div className="name">Angular JS</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+
+      <div className="item">
+        <div className="name">Next JS</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+      <div className="item">
+        <div className="name">Node JS</div>
+        <div className="evaluate" style={{ "--point": "50%" } as React.CSSProperties}></div>
+      </div>
+    </div>
+  </div>
   )
 }
 
